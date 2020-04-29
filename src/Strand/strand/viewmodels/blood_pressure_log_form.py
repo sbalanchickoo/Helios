c393ccaa9e223743cc300b3e055cca6cb81051f5
sbalanchickoo <sbalanchickoo@gmail.com>
2020-04-29 19:35:04 +0100

    working css setup

--- src/Strand/strand/viewmodels/blood_pressure_log_form.py
+++ src/Strand/strand/viewmodels/blood_pressure_log_form.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 
 class BloodPressureForm(Form):
-    systolic = IntegerField(label='Blood pressure (systolic) '
+    systolic = IntegerField(label='Systolic '
                                , validators=[DataRequired(), NumberRange(min=0, max=200)])
-    diastolic = IntegerField(label='Blood pressure (diastolic) '
+    diastolic = IntegerField(label='Diastolic '
                                     , validators=[DataRequired(), NumberRange(min=0, max=200)])
     heart_rate = IntegerField(label='Heart rate '
                              , validators=[DataRequired(), NumberRange(min=0, max=100)])
